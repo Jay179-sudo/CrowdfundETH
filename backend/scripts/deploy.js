edcf3dbdad7229a3feddcb8f0d8dfbf8d9114561
Jay179-sudo <jayprasad2002@gmail.com>
2022-12-19 21:40:10 +0530

    Implemented a basic backend of the contract

---
+++ backend/scripts/deploy.js
@@ -0,0 +1,27 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+const { ethers } = require("hardhat");
+const hre = require("hardhat");
+
+const main = async () =>{
+  const CrowdfundFactory = await hre.ethers.getContractFactory("Crowdfund");
+  const CrowdfundContract = await CrowdfundFactory.deploy();
+  await CrowdfundContract.deployed();
+  console.log("Contract deployed to: ", CrowdfundContract.address);
+}
+
+const runMain = async() =>{
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+}
+
+runMain();
